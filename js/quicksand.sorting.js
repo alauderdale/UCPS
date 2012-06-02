fe913ff645be82a80711c2a5390e7c8046c827c2
Alex Lauderdale <alauderdale@mac.com>
2012-06-02 09:34:00 -0600

    damn

--- js/quicksand.sorting.js
+++ js/quicksand.sorting.js
@@ -1,69 +1,79 @@
-// Custom sorting plugin
-(function($) {
-  $.fn.sorted = function(customOptions) {
-    var options = {
-      reversed: false,
-      by: function(a) { return a.text(); }
-    };
-    $.extend(options, customOptions);
-    $data = $(this);
-    arr = $data.get();
-    arr.sort(function(a, b) {
-      var valA = options.by($(a));
-      var valB = options.by($(b));
-      if (options.reversed) {
-        return (valA < valB) ? 1 : (valA > valB) ? -1 : 0;				
-      } else {		
-        return (valA < valB) ? -1 : (valA > valB) ? 1 : 0;	
-      }
-    });
-    return $(arr);
-  };
-})(jQuery);
+$(document).ready(function(){
+	
+	var items = $('#stage li'),
+		itemsByTags = {};
+	
+	// Looping though all the li items:
+	
+	items.each(function(i){
+		var elem = $(this),
+			tags = elem.data('tags').split(',');
+		
+		// Adding a data-id attribute. Required by the Quicksand plugin:
+		elem.attr('data-id',i);
+		
+		$.each(tags,function(key,value){
+			
+			// Removing extra whitespace:
+			value = $.trim(value);
+			
+			if(!(value in itemsByTags)){
+				// Create an empty array to hold this item:
+				itemsByTags[value] = [];
+			}
+			
+			// Each item is added to one array per tag:
+			itemsByTags[value].push(elem);
+		});
+		
+	});
 
-// DOMContentLoaded
-$(function() {
+	// Creating the "Everything" option in the menu:
+	createList('Everything',items);
 
-  // bind radiobuttons in the form
-  var $filterType = $('#filter input[name="type"]');
-  var $filterSort = $('#filter input[name="sort"]');
+	// Looping though the arrays in itemsByTags:
+	$.each(itemsByTags,function(k,v){
+		createList(k,v);
+	});
+	
+	$('#filter a').live('click',function(e){
+		var link = $(this);
+		
+		link.addClass('active').siblings().removeClass('active');
+		
+		// Using the Quicksand plugin to animate the li items.
+		// It uses data('list') defined by our createList function:
+		
+		$('#stage').quicksand(link.data('list').find('li'));
+		e.preventDefault();
+	});
+	
+	$('#filter a:first').click();
+	
+	function createList(text,items){
+		
+		// This is a helper function that takes the
+		// text of a menu button and array of li items
+		
+		// Creating an empty unordered list:
+		var ul = $('<ul>',{'class':'hidden'});
+		
+		$.each(items,function(){
+			// Creating a copy of each li item
+			// and adding it to the list:
+			
+			$(this).clone().appendTo(ul);
+		});
 
-  // get the first collection
-  var $applications = $('#applications');
+		ul.appendTo('#container');
 
-  // clone applications to get a second collection
-  var $data = $applications.clone();
-
-  // attempt to call Quicksand on every form change
-  $filterType.add($filterSort).change(function(e) {
-    if ($($filterType+':checked').val() == 'all') {
-      var $filteredData = $data.find('li');
-    } else {
-      var $filteredData = $data.find('li[data-type=' + $($filterType+":checked").val() + ']');
-    }
-
-    // if sorted by size
-    if ($('#filter input[name="sort"]:checked').val() == "size") {
-      var $sortedData = $filteredData.sorted({
-        by: function(v) {
-          return parseFloat($(v).find('span[data-type=size]').text());
-        }
-      });
-    } else {
-      // if sorted by name
-      var $sortedData = $filteredData.sorted({
-        by: function(v) {
-          return $(v).find('strong').text().toLowerCase();
-        }
-      });
-    }   
-
-    // finally, call quicksand
-    $applications.quicksand($sortedData, {
-      duration: 600,
-      easing: 'easeInOutQuad'
-    });
-
-  });
-
+		// Creating a menu item. The unordered list is added
+		// as a data parameter (available via .data('list'):
+		
+		var a = $('<a>',{
+			html: text,
+			href:'#',
+			data: {list:ul}
+		}).appendTo('#filter');
+	}
 });
