748a409c6fa0cf74789457d67c71d79c20ab994a
Alex Lauderdale <alauderdale@mac.com>
2012-06-02 10:21:21 -0600

    isotopr

---
+++ js/isotope.config.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+// cache container
+var $container = $('ul#container');
+// initialize isotope
+$container.isotope({
+  // options...
+});
+
+// filter items when filter link is clicked
+$('#filters a').click(function(){
+  var selector = $(this).attr('data-filter');
+  $container.isotope({ filter: selector });
+  return false;
+});
+
+
+
+
+});
